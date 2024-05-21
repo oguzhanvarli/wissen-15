cd8d3e34bd6fd4916499b117844d1691bff5cc1f
Oğuzhan Varlı <106310128+oguzhanvarli@users.noreply.github.com>
2024-05-21 21:51:15 +0300

    21 Mayıs günsonu

---
+++ frontend/first/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header'
+import Main from "./components/Main"
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
