ea603cddd6bdedaffab9f00a8a92057369c496b2
Oğuzhan Varlı <106310128+oguzhanvarli@users.noreply.github.com>
2024-05-30 20:23:59 +0300

    30 mayıs ders ortası

--- frontend/market/src/pages/Login.js
+++ frontend/market/src/pages/Login.js
@@ -2,58 +2,79 @@
 
 
 
+
 import React, { useState } from 'react'
 import { Button, TextField } from "@mui/material"
 import axios from 'axios'
 import SendIcon from '@mui/icons-material/Send';
+import { Formik } from "formik"
+import * as Yup from "yup"
+import {useNavigate} from "react-router-dom"
+
+const LoginSchema = Yup.object().shape({
+  username : Yup.string().required("Username is required!")
+  .min(3, "Username is too short!").max(20, "Username is too long!"),
+  password: Yup.string().required("Password is required!").min(4, "Password is too short!")
+})
 
 
 function Login() {
 
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
+  const navigate = useNavigate()
 
-  console.log(username)
-
-  const handleLogin = async() => {
+  const handleLogin = async (loginObject) => {
     try {
-      if(username != "" || password != ""){
-        let requestObj = {
-          username:username,
-          password:password
-        }
-        let response = await axios.post("http://localhost:9000/user/login",requestObj)
-        console.log(response.data)
+      let response = await axios.post("http://localhost:9000/user/login",loginObject)
+      console.log(response.data)
+      if(response.data.status){
+        navigate("/")
       }
     } catch (error) {
-      console.log('Login Error', error.response.data.message)
-      setUsername("")
-      setPassword("")
+      console.log(error.response)
     }
   }
 
   return (
     <div className='flex justify-center items-center h-screen'>
-      <div>
-        <div>
-          <TextField
-          variant='standard' 
-          label="Username" 
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-          />
-        </div>
-        <div className='my-4'>
-          <TextField variant='standard'
-           label="Password"
-            type='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            />
-        </div>
-        <div>
-          <Button variant='outlined' onClick={handleLogin} endIcon={<SendIcon/>}>Login</Button>
-        </div>
+      <div style={{ minWidth: "300px" }}>
+        <Formik
+          initialValues={{ username: "", password: "" }}
+          onSubmit={(value) => handleLogin(value)}
+          validationSchema={LoginSchema}
+        >
+          {({ values, handleChange, handleSubmit, handleBlur, errors, touched }) => (
+            <>
+              <div>
+                <TextField
+                  variant='standard'
+                  label="Username"
+                  fullWidth
+                  value={values.username}
+                  onChange={handleChange("username")}
+                  onBlur={handleBlur("username")}
+                  error={touched.username && Boolean(errors.username)}
+                  helperText={touched.username && errors.username}
+                />
+              </div>
+              <div className='my-4'>
+                <TextField variant='standard'
+                  label="Password"
+                  type='password'
+                  fullWidth
+                  value={values.password}
+                  onChange={handleChange("password")}
+                  onBlur={handleBlur("password")}
+                  error={touched.password && Boolean(errors.password)}
+                  helperText={touched.password && errors.password}
+                />
+              </div>
+              <div className='flex justify-center'>
+                <Button variant='outlined' onClick={handleSubmit} endIcon={<SendIcon />}>Login</Button>
+              </div>
+            </>
+          )}
+
+        </Formik>
       </div>
 
     </div>
